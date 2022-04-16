02c840bfaa29c0b701f7c8a77dd98ef10f745352
Sara-GS <sara.gonzalezsa@gmail.com>
2022-04-16 08:40:40 +0200

    created tables

--- prueba-tecnica/resources/views/home.blade.php
+++ prueba-tecnica/resources/views/home.blade.php
@@ -3,14 +3,26 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Dashboard</h3>
+            <h3 class="page__heading">Panel de control</h3>
         </div>
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="text-center">Dashboard Content</h3>
+                            <h3 class="text-center">Control de contenidos</h3>
+                        </div>
+                        <div class="card">
+                        <div class="card-body">
+                            <h3 class="text-center">Usuarios</h3>
+                        </div>
+                        <div class="card">
+                        <div class="card-body">
+                            <h3 class="text-center">Cursos</h3>
+                        </div>
+                        <div class="card">
+                        <div class="card-body">
+                            <h3 class="text-center">Compras</h3>
                         </div>
                     </div>
                 </div>
